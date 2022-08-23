e96b15715a11d370f5bf6f6cd1f19b01937c8e22
ARyback <adam.ryback@gmail.com>
2022-08-23 14:46:56 -0500

    Made Info class

---
+++ Models/Info.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplicationOne.Models
+{
+    public class Info
+    {
+        public int Id { get; set; }
+        public string InformationQuestion { get; set; }
+        public string Information { get; set; }
+    }
+}
